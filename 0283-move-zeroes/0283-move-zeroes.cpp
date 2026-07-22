9880e62d841b15fe7f542e918c3a212a28c4bcf1
Muhammad Imran Butt <imrancodes0546@gmail.com>
2026-07-22 19:46:48 +0500

    Time: 0 ms (100%), Space: 23.9 MB (19.47%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,18 +2,28 @@ class Solution {
 public:
  void moveZeroes(vector<int>& nums) {
 
-        int end = nums.size() - 1;
-        for(int i = 0; i < end; i++) {
-            if (nums[i] == 0) {
-                for (int j = i; j < nums.size(); j++) {
-                    if (j + 1 < nums.size()) {
-                        nums[j] = nums[j + 1];
-                    }
-                }
-                nums[nums.size() - 1] = 0;
+        // int end = nums.size() - 1;
+        // for(int i = 0; i < end; i++) {
+        //     if (nums[i] == 0) {
+        //         for (int j = i; j < nums.size(); j++) {
+        //             if (j + 1 < nums.size()) {
+        //                 nums[j] = nums[j + 1];
+        //             }
+        //         }
+        //         nums[nums.size() - 1] = 0;
 
-                i--;
-                end--;
+        //         i--;
+        //         end--;
+        //     }
+        // }
+        // that was O(n^2)
+
+        int j = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
             }
         }
     }
